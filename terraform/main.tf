c1824b56e3b5a0cb467d50020eb7005e1baf3d2c
puneetbhatia77 <5698204+puneetbhatia77@users.noreply.github.com>
2024-07-09 18:49:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -133,7 +133,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "22.04-LTS"
     version   = "latest"
   }
 
